5d671c49affcad61a2401927afc0e4e4c4de6aee
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2021-12-05 06:48:13 +0100

    chore: clean up css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Notes on software development</title>
         <meta name="description" content="Mikhail Vasin's blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>Notes on software development</h1>
-      <p style={{ textAlign: 'center' }}>Work in progress. Stay tuned...</p>
+      <h1>Notes on software development</h1>
+      <p>Work in progress. Stay tuned...</p>
     </div>
   )
 }
