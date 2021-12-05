c76f7de57993823cbe7e6d4ef922b4e5942cb2ff
Mikhail Vasin <wit_slots.0v@icloud.com>
2021-12-05 07:50:33 +0100

    chore: introduce tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="prose">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
