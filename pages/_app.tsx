e85197f6c6fd69722b18bae1cb0a2d380451ce8f
Mikhail Vasin <wit_slots.0v@icloud.com>
2021-12-06 09:22:15 +0100

    improve layout and make Link component pass href to a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import 'tailwindcss/tailwind.css'
+import './tailwind-overrides.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
+// @ts-ignore
+import {MDXProvider} from '@mdx-js/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="prose">
+    <div className="prose p-6">
+      <MDXProvider components={mdComponents}>
       <Component {...pageProps} />
+      </MDXProvider>
     </div>
   )
+}
+
+const mdComponents = {
+  a: Link
 }
 
 export default MyApp
